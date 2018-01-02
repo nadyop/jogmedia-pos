e4cd10f5ee56a9bd4ba4f25e1a0944cf2d343386
Nadya Prabaningrum <nadya.prabaningrum@mail.ugm.ac.id>
2018-01-02 21:41:27 +0700

    set

--- src/main/java/com/blibli/controller/LoginController.java
+++ src/main/java/com/blibli/controller/LoginController.java
@@ -2,15 +2,17 @@ package com.blibli.controller;
 
 import com.blibli.services.EmployeeServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class LoginController {
 
     @RequestMapping("/login")
     public String login() {
-        return "login";
+        return "/login";
     }
 
     @GetMapping("/403")
